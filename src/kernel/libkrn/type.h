61cd8d1f5a57f017736ae3c1baf910f9cfac53f3
Annett Hossain <annetthossain@outlook.com>
2025-12-02 16:22:55 +0600

    update Result type; add Option type

--- src/kernel/libkrn/type.h
+++ src/kernel/libkrn/type.h
@@ -8,10 +8,16 @@
 template<typename T, typename P>
 struct Result {
   union val {
-    T val;
-    P err;
+    T* val;
+    P* err;
   };
-  BOOL has_value;
+  BOOL is_error;
+};
+
+template<typename T>
+struct Option {
+  T* val;
+  BOOL is_valid;
 };
 
 #endif
