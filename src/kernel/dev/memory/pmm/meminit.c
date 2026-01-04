562e82d5569ed6ee369d86adef1f0942ad446b5f
Annett Hossain <annetthossain@outlook.com>
2026-01-04 21:11:27 +0600

    update

--- src/kernel/dev/memory/pmm/meminit.c
+++ src/kernel/dev/memory/pmm/meminit.c
@@ -2,8 +2,15 @@
 #include "phy_ mem.h"
 #include <stdint.h>
 #include <core/kern64.h>
+#include <stddef.h>
 
 bool init_memory(volatile struct limine_memmap_request *memmap_req)
 {
+  if (memmap_req == NULL) return false;
+  if (memmap_req->response == NULL) return false;
+  if (memmap_req->response->entries == NULL) return false;
+
+
+
   return true;
 }
